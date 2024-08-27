eade57268717fef2c551b523047b98eeecaa9856
Mario Chavez <mario_chavez@epam.com>
2024-08-26 22:59:55 -0300

    First Approach for UI Testing

---
+++ ui-tests/tests/page-objects/dashboard/DashboardPage.js
@@ -0,0 +1,45 @@
+// cypress/pages/DashboardPage.js
+
+const BasePage = require('./BasePage'); // Import the base page
+
+class DashboardPage extends BasePage {
+  constructor() {
+    super();
+    // Selectors for elements on the dashboard page
+    this.selectors = {
+      mainPageWrapper: '[class*="pageSwitcher__pageSwitcher"]',
+      reportList: '#reportList',
+      reportItems: '.report-item',
+      addReportButton: '#addReportButton',
+      logoutButton: '#logoutButton'
+    };
+  }
+
+  // Method to verify if the dashboard page is loaded
+  isDashboardPageLoaded() {
+    cy.get(this.selectors.mainPageWrapper).should('be.visible');
+  }
+
+  // Method to click on the add report button
+  clickAddReportButton() {
+    cy.get(this.selectors.addReportButton).click();
+  }
+
+  // Method to get the list of reports displayed
+  getReports() {
+    return cy.get(this.selectors.reportItems);
+  }
+
+  // Method to logout from the application
+  logout() {
+    cy.get(this.selectors.logoutButton).click();
+  }
+
+  // Method to navigate to a specific report
+  navigateToReport(reportId) {
+    const reportSelector = `#report-${reportId}`; // Example selector for specific report
+    cy.get(reportSelector).click();
+  }
+}
+
+module.exports = new DashboardPage();
---
+++ ui-tests/tests/page-objects/login/LoginPage.js
@@ -0,0 +1,70 @@
+// cypress/pages/LoginPage.js
+
+const BasePage = require('./BasePage'); // Import the base page
+
+class LoginPage extends BasePage {
+  constructor() {
+    super();
+    // Selectors for elements on the login page
+    this.selectors = {
+      usernameInput: 'input[name="login"]',
+      passwordInput: 'input[name="password"]',
+      loginButton: '[class*="loginForm__login-button"] button',
+      userAvatarIcon: '[class*="userBlock__avatar-wrapper"]',
+      userMenuContainer: '[class*="userBlock__menu"]',
+      logoutButton: 'button:contains("Logout")', // Adjusted to use a different CSS selector
+      notificationContainer: '.notification-transition-enter-done p',
+      loginSuccessfulMessage: '[class*="notificationItem__success"]'
+    };
+  }
+
+  // Method to navigate to the login page
+  navigateToLoginPage() {
+    this.navigateTo('/#login');
+  }
+
+  // Method to perform login action
+  login(username, password) {
+    this.enterUsername(username);
+    this.enterPassword(password);
+    this.clickLoginButton();
+  }
+
+  // Method to perform logout action
+  logout() {
+    cy.get(this.selectors.userAvatarIcon).click();
+    cy.get(this.selectors.userMenuContainer).find(this.selectors.logoutButton).click();
+  }
+
+  // Method to enter username
+  enterUsername(username) {
+    cy.get(this.selectors.usernameInput).clear().type(username);
+  }
+
+  // Method to enter password
+  enterPassword(password) {
+    cy.get(this.selectors.passwordInput).clear().type(password);
+  }
+
+  // Method to click the login button
+  clickLoginButton() {
+    cy.get(this.selectors.loginButton).click();
+  }
+
+  // Method to get notification container message text
+  getNotificationContainerMessage() {
+    return cy.get(this.selectors.notificationContainer).invoke('text');
+  }
+
+  // Method to verify if the error message is displayed
+  isErrorMessageDisplayed() {
+    return cy.get(this.selectors.notificationContainer).should('be.visible');
+  }
+
+  // Method to verify if the successful login message is displayed
+  isSuccessfulLoginMessageDisplayed() {
+    return cy.get(this.selectors.loginSuccessfulMessage).should('be.visible');
+  }
+}
+
+module.exports = new LoginPage();
