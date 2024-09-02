09b8e92a79c05a0c6114e8300c53d0a37cd7a81b
Mario Chavez <mario_chavez@epam.com>
2024-09-02 19:22:40 -0300

    Adding schema validation for API testing suite

--- api-tests/specs/auth.spec.ts
+++ api-tests/specs/auth.spec.ts
@@ -4,6 +4,23 @@ import AuthEndpoints from '../endpoints/auth';
 import { expect } from '@jest/globals';
 import { Utils } from '../../utils/Utils';
 
+import Joi from "joi";
+
+const authSchema = Joi.object().keys({ 
+  message: Joi.any(),
+  access_token: Joi.string().error(() => 'Access_token field should be a string.'),
+  token_type: Joi.string().error(() => 'Token_type field should be a string.'),
+  refresh_token:  Joi.string().error(() => 'Refresh_token field should be a string.'),
+  expires_in:  Joi.number().error(() => 'Expires_in field should be a number.'),
+  scope:  Joi.string().error(() => 'Scope field should be a string.'),
+  jti: Joi.string().error(() => 'JTI field should be a string.')
+}); 
+
+const errorSchema = Joi.object().keys({
+  errorCode: Joi.number().required().error(() => 'Error code should be a number.'),
+  message: Joi.string().required().error(() => 'Message of error should be a string')
+});
+
 let auth: AuthEndpoints;
 
 describe('Authorization & Authentication', () => {
@@ -18,11 +35,9 @@ describe('Authorization & Authentication', () => {
       password: process.env.API_PASSWORD || "1q2w3e",
       grant_type: "password"
     });
-
     expect(response.status).toEqual(200)
     expect(response.data).not.toBeUndefined();
-    expect(response.data).not.toBeUndefined();
-    expect(response.data).not.toBeUndefined();
+    await Joi.validate(response.data, authSchema)
   })
 
   test('Sign in with not existing credentials', async () => {
@@ -34,7 +49,8 @@ describe('Authorization & Authentication', () => {
       grant_type: "password"
     });
     expect(response.status).toEqual(400)
-    expect(response.data.message).toEqual("You do not have enough permissions. Bad credentials")
+    expect(response.data.message).toEqual("You do not have enough permissions. Bad credentials");
+    await Joi.validate(response.data, errorSchema);
   })
   
 })
