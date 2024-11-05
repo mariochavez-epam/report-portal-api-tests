5216fd2f44936e1c56ad3265e2b4ff03424a9f1e
Mario Chavez <mario_chavez@epam.com>
2024-11-05 00:11:21 -0300

    [API] Adding report portal

--- jest.config.ts
+++ jest.config.ts
@@ -2,31 +2,45 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   "preset": "ts-jest",
-    "testPathIgnorePatterns": [
-      "<rootDir>/ui-tests/*"
-    ],
-    "moduleDirectories": [
-      "node_modules",
-      "<rootDir>"
-    ],
-    maxWorkers: 4,
-    setupFilesAfterEnv: ['<rootDir>/api-tests/config/setup.ts'],
-    reporters: [
-      "default", [
-        "jest-html-reporter", {
-          "outputPath": "<rootDir>/api-tests/reports/test-report.html",
-          "pageTitle": "API Automation Suite",
-          "includeFailureMsg": true
-        }
-      ],
-    ],
-    // This will look for .feature files and their corresponding step definition files.
-    testMatch: [
-      "**/features/**/*.steps.ts"
-    ],
-    transform: {
-      '^.+\\.ts?$': 'ts-jest', // Transform TypeScript files
-    }
+  "testPathIgnorePatterns": [
+    "<rootDir>/ui-tests/*"
+  ],
+  "moduleDirectories": [
+    "node_modules",
+    "<rootDir>"
+  ],
+  maxWorkers: 4,
+  setupFilesAfterEnv: ['<rootDir>/api-tests/config/setup.ts'],
+  reporters: [
+    'default',
+    [
+      '@reportportal/agent-js-jest',
+      {
+        endpoint: "https://rp.epam.com/api/v1",
+        apiKey: process.env.RP_API_KEY,
+        project: "mario_chavez_personal",
+        launch: "API Testing Suite - Report Portal",
+        description: "New Launch for api testing framework using Jest",
+        attributes: [
+          {
+            key: "attributeKey",
+            value: "attrbiuteValue",
+          },
+          {
+            value: "anotherAttrbiuteValue",
+          },
+        ],
+        mode: 'DEFAULT',
+      }
+    ]
+  ],
+  // This will look for .feature files and their corresponding step definition files.
+  testMatch: [
+    "**/features/**/*.steps.ts"
+  ],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest', // Transform TypeScript files
+  }
 };
 
 export default config;
